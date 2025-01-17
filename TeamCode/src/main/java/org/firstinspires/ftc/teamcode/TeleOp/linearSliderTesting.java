1ecefee5fc701827a0eec4255ee7e4030b5b1f99
ajain567 <aadi.jain.la@gmail.com>
2025-01-16 18:33:01 -0800

    Testing Auto
    doesnt work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/linearSliderTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/linearSliderTesting.java
@@ -5,82 +5,48 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.IMU;
 
 @TeleOp(name = "armTesting")
 public class linearSliderTesting extends LinearOpMode {
-    private DcMotor slideRight;
+    private DcMotor armRotatorRight;
 
-    private DcMotor slideLeft;
+    private DcMotor armRotatorLeft;
     public int targetPosition;
 
     @Override
     public void runOpMode() throws InterruptedException {
-        slideLeft = hardwareMap.dcMotor.get("slideL");
-        slideRight = hardwareMap.dcMotor.get("slideR");
+        armRotatorRight = hardwareMap.dcMotor.get("armRR");
+        armRotatorLeft= hardwareMap.dcMotor.get("armRL");
 
-        slideLeft.setDirection(DcMotorEx.Direction.FORWARD);
-        slideRight.setDirection((DcMotorEx.Direction.REVERSE));
+        armRotatorRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        armRotatorLeft.setDirection(DcMotorSimple.Direction.FORWARD);
 
-        slideRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        slideLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armRotatorRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armRotatorLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        slideRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        slideLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        armRotatorRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        armRotatorLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         waitForStart();
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
             // Main control loop
-            telemetry.addData("SlideL", slideLeft.getCurrentPosition());
-            telemetry.addData("SlideR", slideRight.getCurrentPosition());
+            int armRPos = armRotatorRight.getCurrentPosition();
+            int armLPos = armRotatorLeft.getCurrentPosition();
+            telemetry.addData("posR", armRPos);
+            telemetry.addData("posL", armLPos);
+            telemetry.update();
 
             // telemetry.addData("wrist", wrist.getPosition());
-            telemetry.update();
-            slideManual();
+
+
         }
 
     }
-    private void slideManual() {
-        if(slideLeft.getCurrentPosition() < 810 && slideRight.getCurrentPosition() <810 && slideLeft.getCurrentPosition() > -5 && slideRight.getCurrentPosition() > -5 ){
-            if(gamepad2.a) {
-               targetPosition = targetPosition + 30;
 
-           } else if (gamepad2.b) {
-                targetPosition = targetPosition - 30;
-            } else {
-                targetPosition = (slideRight.getCurrentPosition()+slideLeft.getCurrentPosition())/2 ;
-           }
-        } else if(slideLeft.getCurrentPosition() < 810 && slideRight.getCurrentPosition() <810 && !(slideLeft.getCurrentPosition() > -5 && slideRight.getCurrentPosition() > -5)) {
-            if (gamepad2.b) {
-                targetPosition = targetPosition - 30;
-            } else {
-                targetPosition = (slideRight.getCurrentPosition()+slideLeft.getCurrentPosition())/2 ;
-            }
-            if (gamepad2.a) {
-                targetPosition = targetPosition + 30;
-            } else {
-                targetPosition = (slideRight.getCurrentPosition() + slideLeft.getCurrentPosition()) / 2;
-            }
-        } else if(!(slideLeft.getCurrentPosition() < 810 && slideRight.getCurrentPosition() <810) && slideLeft.getCurrentPosition() > -5 && slideRight.getCurrentPosition() > -5) {
-            if (gamepad2.b) {
-                targetPosition = targetPosition - 30;
-            } else {
-                targetPosition = (slideRight.getCurrentPosition()+slideLeft.getCurrentPosition())/2 ;
-            }
-        }   else {
-            targetPosition = (slideRight.getCurrentPosition() + slideLeft.getCurrentPosition()) / 2;
-        }
-//
-        slideRight.setPower(0.5);
-        slideRight.setTargetPosition(targetPosition);
-        slideRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-        slideLeft.setPower(0.5);
-        slideLeft.setTargetPosition(targetPosition);
-        slideLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-    }
 
 
 
